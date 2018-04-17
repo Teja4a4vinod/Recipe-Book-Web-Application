a292c5cd942d61e0dd16350212fd9c8168c338ef
Unknown <utejaswani@gmail.com>
2018-04-17 14:58:17 -0400

    Added items to the shopping list and added recipe details for a single recipe item

--- src/app/shopping-list/shopping-list.component.spec.ts
+++ src/app/shopping-list/shopping-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ShppingListComponent } from './shpping-list.component';
+import { ShoppingListComponent } from './shopping-list.component';
 
-describe('ShppingListComponent', () => {
-  let component: ShppingListComponent;
-  let fixture: ComponentFixture<ShppingListComponent>;
+describe('ShoppingListComponent', () => {
+  let component: ShoppingListComponent;
+  let fixture: ComponentFixture<ShoppingListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShppingListComponent ]
+      declarations: [ ShoppingListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ShppingListComponent);
+    fixture = TestBed.createComponent(ShoppingListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
