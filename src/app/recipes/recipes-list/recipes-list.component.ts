490e2238075d38893dc50b9ec95ff64c33dac6f7
Unknown <utejaswani@gmail.com>
2018-04-17 11:52:58 -0400

    Create Model for recipes.Added recipes to the recipes list using ngFor directive, string interpolation and property binding

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe} from '../recipes.model'
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://www.seriouseats.com/recipes/images/2016/09/20160926-shakshuka-17-1500x1125.jpg'),
+  new Recipe('A Test Recipe', 'This is simply a test', 'https://www.seriouseats.com/recipes/images/2016/09/20160926-shakshuka-17-1500x1125.jpg')
 
+  ];
   constructor() { }
 
   ngOnInit() {
